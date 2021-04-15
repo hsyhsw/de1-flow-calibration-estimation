a8e729b9e4aaa9d1e14f36f6c3ed800bb9671dce
hsyhsw <hsyhsw@hanmail.net>
2021-04-15 20:06:04 +0900

    Added dynamic error series

--- flowcorrection.py
+++ flowcorrection.py
@@ -5,6 +5,7 @@ from typing import TextIO, Dict, List, Tuple
 from scipy import optimize
 from statistics import median
 from functools import partial
+import sys
 import time
 import matplotlib
 matplotlib.use('TkAgg')
@@ -54,11 +55,13 @@ class Shot:
 
 
 class Analysis:
-    def __init__(self, s: Shot):
-        self._diffs = self._calculate_difference(s.time, s.flow, s.weight)
+    def __init__(self, s: Shot, weight_threshold: float = 0.5):
+        self.shot = s
 
-        stable_weight_t = self._stable_weight_time_begin(s.time, s.weight, 0.7)
-        self._initial_window = (stable_weight_t, s.time[-1])
+        self._stable_weight_t = self._stable_weight_time_begin(s.time, s.weight, weight_threshold)
+        self._initial_window = (self._stable_weight_t + 0.5, s.time[-1] - 0.5)
+
+        self._diffs = self._calculate_difference(s.time, s.flow, s.weight, self._stable_weight_t)
 
         self._calculate_optimal_preped = partial(self._estimate_optimal, s.time, s.flow, s.weight, self._diffs)
         self._corr_suggestion = self._calculate_optimal_preped(self._initial_window)
@@ -68,6 +71,7 @@ class Analysis:
         self._x_axis = None
         self._window_fill = None
         self._flow_plt = None
+        self._error_line = None
         self._sugg_line = None
         self._corr_line = None
 
@@ -75,12 +79,17 @@ class Analysis:
         # graph things...
         self._main_fig, self._x_axis = plt.subplots()
         self._x_axis.set_xlabel('seconds')
-        plt.title('Shot at [%s]' % s.shot_time)
+        plt.title('Shot at [%s]' % self.shot.shot_time)
 
-        self._flow_plt, = plt.plot(s.time, s.flow, label='flow (ml/s)', color='blue', lw=3.5)
-        plt.plot(s.time, s.weight, label='weight (g/s)', color='brown', lw=3.5)
-        plt.plot(s.time, s.pressure, label='pressure (bar)', color='green')
-        plt.plot(s.time, [v * 10 for v in self._diffs], label='difference (x10)', color='red')
+        self._flow_plt, = plt.plot(self.shot.time, self.shot.flow, label='flow (ml/s)', color='blue', lw=3.5)
+        plt.plot(self.shot.time, self.shot.weight, label='weight (g/s)', color='brown', lw=3.5)
+        plt.plot(self.shot.time, self.shot.pressure, label='pressure (bar)', color='green')
+        plt.plot(self.shot.time, [v * 10 for v in self._diffs], label='difference (x10)', color='red')
+
+        self._error_line, = plt.plot(self.shot.time, [0.0] * len(self.shot.time), label='error (x10)', color='grey')
+        error_data = self._error_series(self.shot.flow, self._stable_weight_t, 10)
+        self._error_line.set_ydata(error_data)  # little hack to clip large error values
+
         self._sugg_line = plt.axhline(self._corr_suggestion * 10, label='suggestion (x10)', color='pink', linestyle='dashed')
         self._corr_line = plt.axhline(10.0, label='correction (x10)', color='magenta', linestyle='dotted')
 
@@ -91,16 +100,16 @@ class Analysis:
         correction_slider = Slider(correction_ax, 'correction\nvalue', orientation='vertical',
                                    valinit=1.0, valmin=0.3, valmax=2.5, valstep=0.01)
 
-        correction_slider.on_changed(partial(Analysis._update_flow, self, s.flow))
+        correction_slider.on_changed(partial(Analysis._update_flow, self, self.shot.flow))
 
         # window slider
         window_ax = plt.axes([0.16, 0.05, 0.66, 0.03])
         try:
             window_slider = RangeSlider(window_ax, 'opt.\nwindow',
-                                        valinit=self._initial_window, valmin=0.0, valmax=s.time[-1], valstep=0.1)
+                                        valinit=self._initial_window, valmin=0.0, valmax=self.shot.time[-1], valstep=0.1)
         except:  # FIXME: matplotlib bug: move slider to see the correct calculations
             print('ERROR: matplotlib bug: move slider to see the correct calculations.')
-            window_slider = RangeSlider(window_ax, 'opt.\nwindow', valmin=0.0, valmax=s.time[-1], valstep=0.1)
+            window_slider = RangeSlider(window_ax, 'opt.\nwindow', valmin=0.0, valmax=self.shot.time[-1], valstep=0.1)
         window_slider.valtext.set_visible(False)
 
         # to be redrawn on slider move... I know it's dirty!!
@@ -110,8 +119,16 @@ class Analysis:
         self._x_axis.legend()
         plt.show()
 
+    def _error_series(self, target_flow: List[float], from_t: float, magnifier: int) -> List[float]:
+        dat = list()
+        for z in zip(self.shot.time, self.shot.weight, target_flow):
+            dat.append(magnifier * abs(z[1] - z[2]) if from_t <= z[0] else 0.0)
+        return dat
+
     def _update_flow(self, base_flow: List[float], correction_val: float):
-        self._flow_plt.set_ydata([val * correction_val for val in base_flow])
+        new_flow = [val * correction_val for val in base_flow]
+        self._flow_plt.set_ydata(new_flow)
+        self._error_line.set_ydata(self._error_series(new_flow, self._stable_weight_t, 10))
         self._corr_line.set_ydata(correction_val * 10)
         self._main_fig.canvas.draw_idle()
 
@@ -123,17 +140,13 @@ class Analysis:
         self._main_fig.canvas.draw_idle()
 
     @staticmethod
-    def _calculate_difference(t, f, w, calc_threshold=0.5) -> List[float]:
+    def _calculate_difference(t, f, w, from_t: float) -> List[float]:
         diffs = list()
         for z in zip(t, f, w):  # time, flow, weight
-            if z[1] < 0.1 or z[2] < 0.1:
+            if z[0] < from_t or z[1] < 0.1 or z[2] < 0.1:
                 diffs.append(0.0)
             else:  # d = weight / flow
-                if z[1] > calc_threshold and z[2] > calc_threshold:
-                    d = z[2] / z[1]
-                else:  # flow or weight is too low
-                    d = 0.0
-                diffs.append(d)
+                diffs.append(z[2] / z[1])
         return diffs
 
     @staticmethod
@@ -168,4 +181,7 @@ if __name__ == '__main__':
             print('%s doesn\'t seem like a proper shot file.' % shot_file.name)
             exit(2)
         s = Shot.parse(shot_file)
-    Analysis(s).show()
+    if len(sys.argv) > 1:  # optionally first argument being a weight threshold
+        Analysis(s, float(sys.argv[1])).show()
+    else:
+        Analysis(s).show()
