b0691daf54b92026f90e7f55f7303d3fbcb00b9f
Shinyeong Hyun <hsyhsw@gmail.com>
2022-12-06 19:45:29 +0900

    setup.py now lists libs with explicit version specified

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ setup(
     python_requires='>=3.8',
     version='',
     install_requires=[
-        'matplotlib',
-        'scipy',
-        'numpy',
-        'requests',
-        'jupyter',
-        'jupyter-notebookparams',
-        'ipympl',
-        'nb-clean'
+        'matplotlib==3.6.2',
+        'scipy==1.9.3',
+        'numpy==1.23.5',
+        'requests==2.28.1',
+        'jupyter==1.0.0',
+        'jupyter-notebookparams==0.0.4',
+        'ipympl==0.9.2',
+        'nb-clean==2.4.0'
     ],
     py_modules=[
         'flowcorrection',
